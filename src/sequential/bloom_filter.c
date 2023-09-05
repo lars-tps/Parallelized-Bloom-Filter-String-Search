987d8c409ed14bfbd067719fd9d185685eb972e8
HardDisco <49183778+lars-tps@users.noreply.github.com>
2023-09-05 17:29:08 +0800

    Implement bloom_filter_insert

--- src/sequential/bloom_filter.c
+++ src/sequential/bloom_filter.c
@@ -9,14 +9,21 @@ bool * bloom_filter_create_bit_array(bool * bit_arr_ptr, int bit_arr_size){
 }
 
 void bloom_filter_insert(int* bit_arr_ptr, int bit_arr_size, char* str){
-    // Number of hashing function is based on 1,376,656 words of input, 5% false positive rate, and bit array size of 8,583,759
-    int a = _hash1(str, bit_arr_size);
-    int b = _hash2(str, bit_arr_size);
-    int c = _hash3(str, bit_arr_size);
-    int d = _hash4(str, bit_arr_size);
-    int e = _hash5(str, bit_arr_size);
-
     
+    if (!bloom_filter_search(bit_arr_ptr, bit_arr_size, str)){
+        // Number of hashing function is based on 1,376,656 words of input, 5% false positive rate, and bit array size of 8,583,759
+        int a = _hash1(str, bit_arr_size);
+        int b = _hash2(str, bit_arr_size);
+        int c = _hash3(str, bit_arr_size);
+        int d = _hash4(str, bit_arr_size);
+        int e = _hash5(str, bit_arr_size);
+
+        bit_arr_ptr[a] = true;
+        bit_arr_ptr[b] = true;
+        bit_arr_ptr[c] = true;
+        bit_arr_ptr[d] = true;
+        bit_arr_ptr[e] = true;
+    }
 }
 
 bool bloom_filter_search(int* bit_arr_ptr, int bit_arr_size, char* str){
