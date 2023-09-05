7f27ef536b39933cc9b92dd4f423f63ee01b6ee6
HardDisco <49183778+lars-tps@users.noreply.github.com>
2023-09-05 17:24:57 +0800

    Add function comments

--- src/sequential/bloom_filter.h
+++ src/sequential/bloom_filter.h
@@ -1,5 +1,37 @@
 #include <stdbool.h>
 
+/*
+ * Function:  bloom_filter_create_bit_array
+ * --------------------
+ * Creates an array of boolean bits of size bit_arr_size
+ *
+ *  bit_arr_ptr: pointer to array
+ *  bit_arr_size: size of array
+ * 
+ *  returns: An array of boolean bits of size bit_arr_size
+ */
 bool * bloom_filter_create_bit_array(bool * bit_arr_ptr, int bit_arr_size);
+
+/*
+ * Function:  bloom_filter_insert
+ * --------------------
+ * Inserts a string into the bloom filter bit array
+ *
+ *  bit_arr_ptr: pointer to array
+ *  bit_arr_size: size of array
+ *  str: string to insert
+ */
 void bloom_filter_insert(int* bit_arr_ptr, int bit_arr_size, char* str);
+
+/*
+ * Function:  bloom_filter_search
+ * --------------------
+ * Searches for a string in the bloom filter bit array
+ *
+ *  bit_arr_ptr: pointer to array
+ *  bit_arr_size: size of array
+ *  str: string to search for
+ *
+ *  returns: true if string is found, false otherwise
+ */
 bool bloom_filter_search(int* bit_arr_ptr, int bit_arr_size, char* str);
