9ac4b04f0be5893b86493a2d916320744b2bb89f
HardDisco <49183778+lars-tps@users.noreply.github.com>
2023-09-05 22:37:18 +0800

    Add function prototypes for static hash functions, big derp moment

--- src/sequential/bloom_filter.c
+++ src/sequential/bloom_filter.c
@@ -1,13 +1,19 @@
 #include "bloom_filter.h"
 
-bool * bloom_filter_create_bit_array(bool * bit_arr_ptr, int bit_arr_size){
+static int _hash1(char* str, int bit_arr_size);
+static int _hash2(char* str, int bit_arr_size);
+static int _hash3(char* str, int bit_arr_size);
+static int _hash4(char* str, int bit_arr_size);
+static int _hash5(char* str, int bit_arr_size);
+
+bool* bloom_filter_create_bit_array(bool* bit_arr_ptr, int bit_arr_size){
     for(int i = 0; i < bit_arr_size; i++){
         bit_arr_ptr[i] = false;
     }
     return bit_arr_ptr;
 }
 
-void bloom_filter_insert(int* bit_arr_ptr, int bit_arr_size, char* str){
+void bloom_filter_insert(bool* bit_arr_ptr, int bit_arr_size, char* str){
     
     if (!bloom_filter_search(bit_arr_ptr, bit_arr_size, str)){
         // Number of hashing function is based on 1,376,656 words of input, 5% false positive rate, and bit array size of 8,583,759
@@ -25,7 +31,7 @@ void bloom_filter_insert(int* bit_arr_ptr, int bit_arr_size, char* str){
     }
 }
 
-bool bloom_filter_search(int* bit_arr_ptr, int bit_arr_size, char* str){
+bool bloom_filter_search(bool* bit_arr_ptr, int bit_arr_size, char* str){
     int a = _hash1(str, bit_arr_size);
     int b = _hash2(str, bit_arr_size);
     int c = _hash3(str, bit_arr_size);
